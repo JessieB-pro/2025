16f82ce0665d517650df19d14ae89c6ad37874ac
Jess <jess@monadresse.com>
2024-12-30 22:23:34 +0100

    New commit

--- src/components/Enigmes.jsx
+++ src/components/Enigmes.jsx
@@ -1,6 +1,38 @@
+import { useState, useEffect } from "react";
 import berry_1 from '../assets/berry_1.jpg'
 
 export default function Enigmes() {
+
+  const [indice, setIndice] = useState("");
+
+  useEffect(() => {
+    // Fonction pour vérifier l'heure et les minutes et définir l'indice
+    const afficherIndice = () => {
+      const maintenant = new Date();
+      const heures = maintenant.getHours(); // Heures actuelles
+      const minutes = maintenant.getMinutes(); // Minutes actuelles
+
+      // Condition basée sur heures et minutes
+      if ((heures === 20 && minutes >= 0) || (heures === 20 && minutes < 59)) {
+        setIndice("Indice 1 : Additionne tous les chiffres de la nouvelle année...dépêche toi avEnt qu'il ne soit trop tard ;)");
+      } else if ((heures === 21 && minutes >= 0) || (heures === 21 && minutes < 59)) {
+        setIndice("Indice 2 : L'après-midi est parfait pour un café.");
+      } else if ((heures === 22 && minutes >= 0) || (heures === 22 && minutes < 59)) {
+        setIndice("Indice 3 : La nuit porte conseil.");
+      } else {
+        setIndice("Aucun indice pour le moment. Revenez à une autre heure !");
+      }
+    };
+
+    // Appel initial et mise à jour toutes les minutes
+    afficherIndice();
+    const intervalId = setInterval(afficherIndice, 60000);
+
+    // Nettoyer l'intervalle lorsque le composant est démonté
+    return () => clearInterval(intervalId);
+  }, []);
+
+
   return (
     <>
       <div id="enigmes" className="container-fluid">
@@ -18,7 +50,7 @@ export default function Enigmes() {
         </div>
 
         <div className="heading">
-          <h3 className="animate flash mt-5">Indice n°... : .......</h3>
+          <h3 className="animate flash mt-5">{indice}</h3>
         </div>
 
       </div>
