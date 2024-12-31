4101bceb348eaa026e18eba29e40d8e8f255b311
Jess <jess@monadresse.com>
2024-12-31 23:06:14 +0100

    rectif

--- src/components/Enigmes.jsx
+++ src/components/Enigmes.jsx
@@ -19,11 +19,11 @@ export default function Enigmes() {
         setIndice("Indice 2 : Il n’est pas mouillé mais on l’a mis dedans quand même !");
       } else if ((heures === 22 && minutes >= 0) || (heures === 22 && minutes < 59)) {
         setIndice("Indice 3 : 1234567890QWERTYUIOP…");
-      } else if ((heures === 23 && minutes >=5) || (heures === 23 && minutes < 20)) {
+      } else if ((heures === 23 && minutes >=5) || (heures === 23 && minutes < 30)) {
         setIndice("Indice 4 : Sous l'eau");
-      } else if ((heures === 23 && minutes >=21) || (heures === 23 && minutes < 35)) {
+      } else if ((heures === 23 && minutes >=31) || (heures === 23 && minutes < 55)) {
         setIndice("Indice 5 : On coupe le jus");
-      } else if ((heures === 23 && minutes >=36) || (heures === 23 && minutes < 59)) {
+      } else if ((heures === 23 && minutes >=56) || (heures === 23 && minutes < 59)) {
         setIndice("C'est fini !");
       } else {
         setIndice("Aucun indice pour le moment. Revenez à une autre heure !");
